5b06943bf646437dcc947fba8a6f225bd9b0b709
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2024-11-05 08:31:29 -0300

    chore: use ansible to update the html file

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "MyFirstInstance"
+    Name = "BashCodeInstance"
   }
   key_name = "my-first-instance-key"
 }
