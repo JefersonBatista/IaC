60f4b7607af5a4d4ed7d119d81f6272ef3afa56c
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2024-12-19 16:31:31 -0300

    chore: create prd module

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,8 +1,10 @@
 module "aws-dev" {
-  source     = "../../infra"
-  aws_region = "us-east-2"
-  instance   = "t2.micro"
-  key        = "IaC-DEV"
+  source                     = "../../infra"
+  aws_region                 = "us-east-2"
+  instance                   = "t2.micro"
+  key                        = "IaC-DEV"
+  security_group_name        = "general_dev_access"
+  security_group_description = "dev group"
 }
 
 output "dev_public_ip" {
